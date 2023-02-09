72941802aeddb001235117966cb190117828f35b
Lira <bruno.lira@n3urons.com>
2023-02-09 14:40:27 -0300

    feat: add context

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import { Configuration, OpenAIApi } from "openai"
 
 dotenv.config()
 
-const defaultContext = `The following is a conversation with an AI assistant. The assistant is helpful, creative, clever, and very friendly.\n\nHuman: Hello, who are you?\nAI: I am an AI created by OpenAI. How can I help you today?\nHuman: {"vendedor":"andre","vendas":[{"data":"2023-02-01","valor":1200.87},{"data":"2023-02-02","valor":1056.87},{"data":"2023-02-03","valor":1302.87},{"data":"2023-02-04","valor":985.87},{"data":"2023-02-05","valor":1116.87},{"data":"2023-02-06","valor":1250.87},{"data":"2023-02-07","valor":1165.87}],"meta":117983.36,"previsão":456300.11,"clientes":[{"código":"132811","previsao":69517.15,"produtos":[{"descrição":"produto A","quantidade":12},{"descrição":"produto B","quantidade":8},{"descrição":"produto C","quantidade":14}]},{"código":"217974","previsao":62276.46,"produtos":[{"descrição":"produto D","quantidade":13},{"descrição":"produto E","quantidade":18},{"descrição":"produto F","quantidade":5}]},{"código":"210639","previsao":45229.63,"produtos":[{"descrição":"produto G","quantidade":6},{"descrição":"produto H","quantidade":9},{"descrição":"produto I","quantidade":8}]}]}`
+const defaultContext = `The following is a conversation with an AI assistant. The assistant is helpful, creative, clever, and very friendly.\n\nHuman: Hello, who are you?\nAI: I am an AI created by OpenAI. How can I help you today?\nHuman: {"vendedor":"andre", "vendas":[{"data":"2023-02-01","valor":1200.87},{"data":"2023-02-02","valor":1056.87},{"data":"2023-02-03","valor":1302.87},{"data":"2023-02-04","valor":985.87},{"data":"2023-02-05","valor":1116.87},{"data":"2023-02-06","valor":1250.87},{"data":"2023-02-07","valor":1165.87}],"meta":117983.36,"previsão":456300.11,"clientes":[{"código":"132811","previsao":69517.15,"produtos":[{"descrição":"produto A","quantidade":12},{"descrição":"produto B","quantidade":8},{"descrição":"produto C","quantidade":14}]},{"código":"217974","previsao":62276.46,"produtos":[{"descrição":"produto D","quantidade":13},{"descrição":"produto E","quantidade":18},{"descrição":"produto F","quantidade":5}]},{"código":"210639","previsao":45229.63,"produtos":[{"descrição":"produto G","quantidade":6},{"descrição":"produto H","quantidade":9},{"descrição":"produto I","quantidade":8}]}]}`
 
 const contextMap = new Map([])
 
@@ -26,7 +26,7 @@ const openai = new OpenAIApi(configuration);
 
 const getDavinciResponse = async (phoneNumber, clientText) => {
     const parsedClientText = `${clientText}\nAI:`;
-    const contextWithClientText = `${contextMap.get(phoneNumber) || defaultContext}\nHuman: ${parsedClientText}`
+    const contextWithClientText = `${contextMap.get(phoneNumber) || defaultContext}${parsedClientText}`
     const options = {
         model: "text-davinci-003",
         prompt: contextWithClientText,
