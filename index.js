80a229900e679d9a28ae68d0102f92faa91c99b8
Lira <bruno.lira@n3urons.com>
2023-02-09 13:02:26 -0300

    feat: add context

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ import { Configuration, OpenAIApi } from "openai"
 
 dotenv.config()
 
-const contextMap = new Map([
-    // ['558197298151@c.us', `Human:Tell me one famous person's name\nAI:Dwayne Johnson\nHuman:A brazilian one\nAI:Fernando Torres\nHuman:A cuban one\nAI:Celia Cruz\nHuman:A Chinese one\nAI:Jackie Chan`]
-])
+const defaultContext = `The following is a conversation with an AI assistant. The assistant is helpful, creative, clever, and very friendly.\n\nHuman: Hello, who are you?\nAI: I am an AI created by OpenAI. How can I help you today?\nHuman: {"vendedor":"andre","vendas":[{"data":"2023-02-01","valor":1200.87},{"data":"2023-02-02","valor":1056.87},{"data":"2023-02-03","valor":1302.87},{"data":"2023-02-04","valor":985.87},{"data":"2023-02-05","valor":1116.87},{"data":"2023-02-06","valor":1250.87},{"data":"2023-02-07","valor":1165.87}],"meta":117983.36,"previsão":456300.11,"clientes":[{"código":"132811","previsao":69517.15,"produtos":[{"descrição":"produto A","quantidade":12},{"descrição":"produto B","quantidade":8},{"descrição":"produto C","quantidade":14}]},{"código":"217974","previsao":62276.46,"produtos":[{"descrição":"produto D","quantidade":13},{"descrição":"produto E","quantidade":18},{"descrição":"produto F","quantidade":5}]},{"código":"210639","previsao":45229.63,"produtos":[{"descrição":"produto G","quantidade":6},{"descrição":"produto H","quantidade":9},{"descrição":"produto I","quantidade":8}]}]}`
+
+const contextMap = new Map([])
 
 create({
     session: 'Open-Ai',
@@ -25,18 +25,14 @@ const configuration = new Configuration({
 const openai = new OpenAIApi(configuration);
 
 const getDavinciResponse = async (phoneNumber, clientText) => {
-    const messageSize = String(clientText).length;
-    if (messageSize > 1000) {
-        return `❌ ChatBot Response Error: Your message should contain less than 1000 characters. (${messageSize})`
-    }
-    const parsedClientText = clientText;
-    const contextWithClientText = `${contextMap.get(phoneNumber) || ""}${parsedClientText}`
+    const parsedClientText = `${clientText}\nAI:`;
+    const contextWithClientText = `${contextMap.get(phoneNumber) || defaultContext}${parsedClientText}`
     const options = {
         model: "text-davinci-003",
         prompt: contextWithClientText,
         temperature: 1,
-        max_tokens: 3000,
-        // stop: [" Human:", " AI:"],
+        max_tokens: 1024,
+        stop: [" Human:", " AI:"],
     }
 
     try {
@@ -47,7 +43,7 @@ const getDavinciResponse = async (phoneNumber, clientText) => {
             apiTextResponse += text
         })
         const parsedApiTextResponse = `${apiTextResponse.trim()}`
-        const newContext = `${contextWithClientText}${parsedApiTextResponse}`
+        const newContext = `${contextWithClientText}${parsedApiTextResponse}\nHuman: `
         contextMap.set(phoneNumber, newContext)
         console.log('FINAL CONTEXT --> ', contextMap.get(phoneNumber));
         return parsedApiTextResponse;
@@ -81,7 +77,6 @@ const commands = (client, message) => {
     let firstWord = message.text.substring(0, message.text.indexOf(" "));
     const phoneNumber = message.from === process.env.PHONE_NUMBER ? message.to : message.from;
     switch (firstWord) {
-
         case iaCommands.clearContex:
             contextMap.delete(phoneNumber);
             client.sendText(phoneNumber, 'Context deleted :)');
@@ -105,6 +100,13 @@ const commands = (client, message) => {
                 )
             })
             break;
+
+        // default:
+        //     const question = message.text;
+        //     getDavinciResponse(phoneNumber, question).then((response) => {
+        //         client.sendText(phoneNumber, response)
+        //     })
+        //     break;
     }
 }
 
