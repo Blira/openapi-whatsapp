1939f6cc77a0d7de5c256bb5ba10af4dabd2c31f
Lira <bruno.lira@n3urons.com>
2023-02-09 14:46:13 -0300

    feat: add context

--- index.js
+++ index.js
@@ -70,7 +70,7 @@ const getDalleResponse = async (clientText) => {
 const commands = (client, message) => {
     console.log(message)
     const iaCommands = {
-        // davinci3: "/bot",
+        davinci3: "/bot",
         dalle: "/img",
         clearContex: "/cc"
     }
@@ -83,12 +83,12 @@ const commands = (client, message) => {
             client.sendText(phoneNumber, 'Context deleted :)');
             break;
 
-        // case iaCommands.davinci3:
-        //     const question = message.text.substring(message.text.indexOf(" "));
-        //     getDavinciResponse(phoneNumber, question).then((response) => {
-        //         client.sendText(phoneNumber, response)
-        //     })
-        //     break;
+        case iaCommands.davinci3:
+            const question = message.text.substring(message.text.indexOf(" "));
+            getDavinciResponse(phoneNumber, question).then((response) => {
+                client.sendText(phoneNumber, response)
+            })
+            break;
 
         case iaCommands.dalle:
             const imgDescription = message.text.substring(message.text.indexOf(" "));
@@ -102,12 +102,12 @@ const commands = (client, message) => {
             })
             break;
 
-        default:
-            const question = message.text;
-            getDavinciResponse(phoneNumber, question).then((response) => {
-                client.sendText(phoneNumber, response)
-            })
-            break;
+        // default:
+        //     const question = message.text;
+        //     getDavinciResponse(phoneNumber, question).then((response) => {
+        //         client.sendText(phoneNumber, response)
+        //     })
+        //     break;
     }
 }
 
